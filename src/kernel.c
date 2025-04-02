7a84262636fc9be45b7b84aa4336e9ab119f2448
circuitnomad1337 <kubakusnierek@.gmail.com>
2025-04-02 03:25:36 +0200

    Kernel 'hello world' success

--- src/kernel.c
+++ src/kernel.c
@@ -9,15 +9,44 @@ typedef struct multiboot_header {
 
 __attribute__((section(".multiboot"))) multiboot_header_t header = {
     0x1BADB002,        // Magic number
-    0x00000003,        // Flags (memory and video info)
+    0x00000003,        // Flags 
     -(0x1BADB002 + 0x00000003) // Checksum (magic + flags must equal zero)
 };
 
+typedef unsigned short uint16_t;
+
+// VGA text mode memory starts at 0xB8000 <--- memorize it ~jakub
+#define VIDEO_MEMORY ((uint16_t*) 0xB8000)
+#define SCREEN_WIDTH 80
+#define SCREEN_HEIGHT 25
+
 __attribute__((section(".text"))) 
-__attribute__((naked))
+
+void clear_screen() {
+    uint16_t blank = 0x0F00; // WHITE on BLACK 
+
+    for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; i++) {
+        VIDEO_MEMORY[i] = blank;
+    }
+}
+
+void print_string(const char* str) {
+    uint16_t* screen = VIDEO_MEMORY;
+    
+    while (*str) {
+        *screen = (0x0F << 8) | *str;
+        screen++;
+        str++;
+    }
+}
+
+void kernel_main() {
+    clear_screen();
+    print_string("Hello world!\n");
+
+    while (1) {}
+}
 
 void _start() {
-    *(char*)0xB8000 = 'H'; 
-    *(char*)0xB8001 = 0x07; // WHITE ON BLACK
-    while (1) {} 
+    kernel_main();
 }
